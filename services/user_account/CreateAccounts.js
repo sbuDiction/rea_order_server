854d4509eba0d356d7b69a7c196af0f7a3141c07
sbuDiction <nsbu68501@gmail.com>
2021-10-22 16:15:23 +0200

    added some sql

--- services/user_account/CreateAccounts.js
+++ services/user_account/CreateAccounts.js
@@ -1,4 +1,27 @@
-export class { 
+export class {
 
-    create
+    constructor(pool) {
+        this.pool = pool;
+    }
+
+    async createAccount(account) {
+        let checkIfUserExist = await this.pool("SELECT * FROM users WHERE email = $1",
+            [account.email]);
+        if (checkIfUserExist.rows.length === 0) {
+            await this.pool("INSERT INTO users (name, last_name, email, phone_number, user_password, is_admin, verified) VALUES ($1, $2, $3, $4, $5, $6, $7)",
+                [
+                    account.firstName,
+                    account.lastName,
+                    account.email,
+                    account.phoneNumber,
+                    account.password,
+                    account.isAdmin,
+                    account.verified
+                ]);
+            return true;
+        }
+        return false
+    }
+
+
 }
